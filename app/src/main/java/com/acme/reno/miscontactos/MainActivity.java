bc025045028bb5ebe7872f10d3eceb45833dca5f
lectorasidu <lectorasidu@gmail.com>
2015-04-12 18:07:55 +0200

    Paso 3

--- app/src/main/java/com/acme/reno/miscontactos/MainActivity.java
+++ app/src/main/java/com/acme/reno/miscontactos/MainActivity.java
@@ -5,20 +5,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.Toast;
 
+import com.acme.reno.miscontactos.util.Contacto;
 import com.acme.reno.miscontactos.util.TextChangedListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private EditText txtNombre, txtTelefono, txtEmail, txtDireccion;
+    private List<Contacto> contactos = new ArrayList<Contacto>();
+    private ListView contactsListView;
     private Button btnAgregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarComponentesUI();
+        inicializarTabs();
+    }
+
+    private void inicializarTabs() {
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Crear");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("tab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Lista");
+        tabHost.addTab(spec);
     }
 
     private void inicializarComponentesUI() {
@@ -26,7 +49,7 @@ public class MainActivity extends ActionBarActivity {
         txtTelefono=(EditText)findViewById(R.id.cmpTelefono);
         txtEmail=(EditText)findViewById(R.id.cmpEmail);
         txtDireccion=(EditText)findViewById(R.id.cmpDireccion);
-
+        contactsListView=(ListView)findViewById(R.id.listView);
         txtNombre.addTextChangedListener(new TextChangedListener(){
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
---
+++ app/src/main/java/com/acme/reno/miscontactos/util/Contacto.java
@@ -0,0 +1,48 @@
+package com.acme.reno.miscontactos.util;
+
+/**
+ * Created by Reno on 12/04/2015.
+ */
+public class Contacto {
+
+    private String nombre, direccion, telefono, email;
+
+    public Contacto(String nombre, String direccion, String telefono, String email) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
